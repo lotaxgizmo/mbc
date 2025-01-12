892761a4ddd6441bae6f10d923c6e22dc0bfa393
lotaxgizmo <lotaoko2@gmail.com>
2025-01-12 23:45:09 +0100

    Update

--- src/components/Firstvid.js
+++ src/components/Firstvid.js
@@ -5,7 +5,7 @@ import mbcvideo2 from '../assets/mbcvideo2.mp4'
 function Firstvid() {
     return (
         <div className='flex flex-col justify-center items-center px-4s my-20'>
-            <video src={mbcvideo2} autoPlay loop muted playsInline className='w-8/12 rounded-xl border-[0.1px] border-grey lg:my-20 my-10' />
+            <video src={mbcvideo2} autoPlay loop muted playsInline className='lg:w-9/12 w-11/12 rounded-xl dborder-[0.1px] border-greyd lg:my-20 my-10' />
         </div>
     )
 }
